85073881532844b89e5c6b853ed66ed0fefb86f6
pauuandress <pau.andres@estudiantat.upc.edu>
2022-10-17 15:39:22 +0200

    he posat hola l22

--- ss_entry.py
+++ ss_entry.py
@@ -19,7 +19,7 @@ marsParams[1] = marsParams[0] * marsParams[3]
 mercuryParams[1] = - mercuryParams[0] * mercuryParams[3] * np.cos(np.radians(77.4))
 mercuryParams[2] = mercuryParams[0] * mercuryParams[3] * np.sin(np.radians(77.4))
 
-
+#hola
 #Edicio 1
 t = np.linspace(0, 2*pi, 100)
 f = np.linspace(-1.5, 1.5, 100)
