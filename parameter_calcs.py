da6c958efa3cd0caa24eb6cb243a0857ef43a0e6
FireKun8 <aleixribastorras@gmail.com>
2022-10-17 16:02:35 +0200

    seperate calcs

---
+++ parameter_calcs.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+marsParams = [0.093, 0, 0, 1.524, 0]
+mercuryParams = [0.206, 0, 0, 0.387, 0]
+earthParams = [0, 0, 0, 1, 1]
+jupiterParams = [0, 0, 0, 5.2, 5.2]
+venusParams = [0, 0, 0, 0.723, 0.723]
+craftParams = [1.539, 2.72 + 0.8, 0, 2.72, 2.23]
+
--- ss_entry.py
+++ ss_entry.py
@@ -1,18 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import pi
+import parameter_calcs as pc
 
 #Planet parameter list format [eccentricity, x0, y0, major semi-axis, minor-semiaxis]
-marsParams = [0.093, 0, 0, 1.524, 0]
-mercuryParams = [0.206, 0, 0, 0.387, 0]
-earthParams = [0, 0, 0, 1, 1]
-jupiterParams = [0, 0, 0, 5.2, 5.2]
-venusParams = [0, 0, 0, 0.723, 0.723]
-craftParams = [1.539, 2.72 + 0.8, 0, 2.72, 2.23]
+marsParams = pc.marsParams
+mercuryParams = pc.mercuryParams
+earthParams = pc.earthParams
+jupiterParams = pc.jupiterParams
+venusParams = pc.venusParams
+craftParams = pc.craftParams
 
 #MARS and MERCURY minor semi-axis
 marsParams[4] = np.sqrt(np.power(marsParams[3], 2) - np.power(marsParams[3], 2) * np.power(marsParams[0], 2))
 mercuryParams[4] = np.sqrt(np.power(mercuryParams[3], 2) - np.power(mercuryParams[3], 2) * np.power(mercuryParams[0], 2))
+
 
 #MARS and MERCURY orbit focus
 marsParams[1] = marsParams[0] * marsParams[3]
