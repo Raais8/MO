478771a50ee947c373e3f51f938073707b377b9d
pauuandress <pau.andres@estudiantat.upc.edu>
2022-10-20 23:03:27 +0200

    craft

--- parameter_calcs.py
+++ parameter_calcs.py
@@ -1,3 +1,4 @@
+from cmath import sqrt
 import numpy as np
 
 #Planet parameter list format [eccentricity, x0, y0, major semi-axis, minor-semiaxis]
@@ -6,7 +7,7 @@ mercuryParams = [0.206, 0, 0, 0.387, 0]
 earthParams = [0, 0, 0, 1, 1]
 jupiterParams = [0, 0, 0, 5.2, 5.2]
 venusParams = [0, 0, 0, 0.723, 0.723]
-craftParams = [1.539, 2.72 + 0.8, 0, 2.72, 2.23]
+craftParams = [0, 0, 0, 0, 0]
 
 #MARS and MERCURY minor semi-axis
 marsParams[4] = np.sqrt(np.power(marsParams[3], 2) - np.power(marsParams[3], 2) * np.power(marsParams[0], 2))
@@ -16,3 +17,10 @@ mercuryParams[4] = np.sqrt(np.power(mercuryParams[3], 2) - np.power(mercuryParam
 marsParams[1] = marsParams[0] * marsParams[3]
 mercuryParams[1] = - mercuryParams[0] * mercuryParams[3] * np.cos(np.radians(77.4))
 mercuryParams[2] = mercuryParams[0] * mercuryParams[3] * np.sin(np.radians(77.4))
+
+#CRAFT eccentricity and semi-axis
+craftParams[0] = 8.8 / 6.8
+craftParams[3] = 0.8 / (craftParams[0] - 1)
+craftParams[1] = 0.8 + craftParams[3]
+c = craftParams[3] * craftParams[0]
+craftParams[4] = sqrt (np.power(c,2) - np.power(craftParams[3],2))
